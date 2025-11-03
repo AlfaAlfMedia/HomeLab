8890a1849522be7e69fd5f00c51f40d0389dfd6b
AlfaAlfMedia <128143652+AlfaAlfMedia@users.noreply.github.com>
2025-11-03 21:41:14 +0100

    Update domain and email placeholders in script

--- scripts/certificates/certbot-hetzner-cloud.sh
+++ scripts/certificates/certbot-hetzner-cloud.sh
@@ -11,7 +11,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Konfiguration
-DOMAIN="alfaalf-media.de"
+DOMAIN="deine.domain"
 EMAIL="deine@email.de"
 HETZNER_API_TOKEN="DEIN_CLOUD_TOKEN_HIER"
 VENV_DIR="/opt/certbot"
